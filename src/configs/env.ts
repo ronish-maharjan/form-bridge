351ab54e327a33aa2adb7a5e0420a96feaa01f20
ronishmaharjan <ronish-maharjan@users.noreply.github.com>
2026-07-04 21:27:55 +0545

    update(env): update env     - update proper naming convention     - add new envs

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,9 +1,25 @@
 import dotenv from "dotenv"
+import z from "zod";
 dotenv.config();
 
-const env = {
-    connectionString:process.env.DATABASE_CONNECTION_URL,
-    port:process.env.PORT
+const envSchema = z.object({
+    CONNECTION_STRING:z.string().trim().min(8),
+    PORT:z.coerce.number(),
+    JWT_SECRET_KEY:z.string().trim().min(8)
+})
+
+const envData = {
+    CONNECTION_STRING:process.env.DATABASE_CONNECTION_URL,
+    PORT:process.env.PORT,
+    JWT_SECRET_KEY:process.env.JWT_SECRET_KEY
 }
+
+const parsedEnv = envSchema.safeParse(envData)
+
+if(!parsedEnv.success){
+    console.log(parsedEnv.error.flatten().fieldErrors)
+    process.exit(1)
+}
+const env = parsedEnv.data
 export {env};
 
