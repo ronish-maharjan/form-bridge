7b4d56de55ac2889efc6d545f56c92b8cbf5450d
ronishmaharjan <ronish-maharjan@users.noreply.github.com>
2026-07-15 18:05:14 +0545

    fix(logout): fix cookie not removing when logout     - add proper creation and deletion properties in cookie

--- src/modules/auth/presentation/http/controllers/logout.controller.ts
+++ src/modules/auth/presentation/http/controllers/logout.controller.ts
@@ -1,15 +1,24 @@
 import { NextFunction, Request, Response } from "express";
+import { logger } from "../../../../../shared/logger/logger.js";
 
 class LogoutController {
-  public handle = async (req: Request, res: Response, next: NextFunction) => {
-    res.clearCookie("__secure-access-token", {
-      path: "/",
-    });
-    res.clearCookie("__secure-refresh-token", {
-      path: "/",
-    });
-    return res.sendStatus(204);
-  };
+    public handle = async (req: Request, res: Response, next: NextFunction) => {
+        logger.debug({data:req.cookies["__Secure-access-token"]})
+        res.clearCookie("__Secure-access-token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            path: "/",
+        });
+
+        res.clearCookie("__Secure-refresh-token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            path: "/",
+        });
+        return res.sendStatus(204);
+    };
 }
 
 export { LogoutController };
