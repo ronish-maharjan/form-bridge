c9977e65e29581af39637cca3786bb780d9d3556
ronishmaharjan <ronish-maharjan@users.noreply.github.com>
2026-07-18 13:05:38 +0545

    feat(newRoute): add global route handler and add landing pageroute     - currently serving static landing page in root route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request ,Response} from "express";
+import path from "node:path";
 import { router } from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import { globalErrorHandler } from "./shared/middlewares/global-error-handler.js";
@@ -13,9 +14,25 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+
+// main landing page route
+// currently serving basic landing page :)
+app.get("/", (req:Request, res:Response) => {
+  res.sendFile(path.join(process.cwd(), "assets", "index.html"));
+});
+
+// Main routes of our application
 app.use("/api", router);
+
+// Api documentation route
 app.use( "/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Catch all routes
+app.all("/{*path}", (req:Request, res:Response) => {
+  res.status(404).send("404 Not Found");
+});
+
+// Global Error handler
 app.use(globalErrorHandler);
 
 export {app};
